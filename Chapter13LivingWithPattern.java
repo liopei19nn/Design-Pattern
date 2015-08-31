c94247f73667cdced061e891548689abd0c73a7d
lip <liopei19nn@gmail.com>
2015-08-31 14:30:47 -0400

    add chapter13 final Chapter

---
+++ Chapter13LivingWithPattern.java
@@ -0,0 +1,58 @@
+// A pattern is a solution to a problem in a context
+
+// Context is the recurring situation of problem
+
+// Problem should be goal and restraints
+
+// Solution should be a general solution to resolve this kind of problem
+
+
+
+// Classification of pattern
+
+// Approach 1
+
+// Creational Pattern :
+// Involve object instantiation and all provide a way
+// to decouple a client from the objctes it needs to instantiate
+
+// Singleton, Builder, Prototype, Abstract Factory, Factory Method
+
+
+
+// Behavioral Pattern :
+// Any pattern that is a Behavioral Pattern is concerned with how classes
+// and objects interact and distributed responsibility
+
+// Mediator, Visitor, Template Method, Iterator, Command, Memento, Interpreter, Observer
+// Chain of Responsibility, State, Strategy
+
+
+
+// Structual Pattern :
+// Compose classes or objects into larger structures
+
+// Decorator, Composite, Proxy, Facade, Bridge, Flyweight, Adapter
+
+
+
+
+
+// Approach 2
+
+// Class Patterns :
+
+// Describe how relationship between classes are defined via inheritance. Relationships in class
+// patterns are established at compile time
+
+// Template method, factory method, adapter, interpreter
+
+// Object Patterns : 
+// Object patterns describe relationships between objects and are primarily defined by composition. Relationships in object patterns are typically created at runtime and
+// are more dynamic and flexible.
+
+// all the other could be classified as Object Patterns
+
+// Anti-Pattern : tells you how to go from a problem to a BAD solution
+
+// Anti-Pattern will tell you why a solution is attractive, and long-term bad, and suggest other pattern
